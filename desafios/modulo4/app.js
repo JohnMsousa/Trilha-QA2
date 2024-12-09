3226b6924681476228f84db169bd83beb865b0a7
JohnMsousa <johnmsousasantos@gmail.com>
2024-12-09 10:17:15 -0300

    feat: exercícios de manipulação de listas em JavaScript
    - Criação e modificação de listas (linguagensDeProgramacao, usuarios, objetos, times).
    - Exibição de elementos específicos das listas no console.

---
+++ desafios/modulo4/app.js
@@ -0,0 +1,22 @@
+// 1 Crie uma lista vazia, com o nome listaGenerica.
+
+let listaGenerica = [];
+
+// 2 Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+// 3 Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+
+// 4 Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let usuarios = ["John", "Lucas", "Arnaldo"];
+console.log(usuarios[0] + ".");
+
+// 5 Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let objetos = ["Caneta", "Controle", "Pulseira"];
+console.log(objetos[1] + ".");
+
+// 6 Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let times = ["Fortaleza", "São Paulo", "Palmeiras"];
+console.log(times[times.length - 1] + ".");
